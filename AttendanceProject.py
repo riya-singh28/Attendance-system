ef34ab2b8f6b635b88959a4b44b218bed7222153
RIYA SINGH <98592805+riya-singh28@users.noreply.github.com>
2022-05-28 10:23:26 +0530

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -3,44 +3,106 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
+import pandas as pd
 
-from PIL import ImageGrab
+#from PIL import ImageGrab
 
-path = 'ImagesAttendance'
+path = 'images'
 images = []
-classNames = []
+personName = []
 myList = os.listdir(path)
 print(myList)
-for cls in myList:
-    curImg = cv2.imread(f'{path}/{cls}')
-    images.append(curImg)
-    classNames.append(os.path.splitext(cls)[0])
-print(classNames)
+for cu_img in myList:
+    current_Img = cv2.imread(f'{path}/{cu_img}')
+    images.append(current_Img)
+    personName.append(os.path.splitext(cu_img)[0])
+print(personName)
 
 
-def findEncodings(images):
+
+
+def faceEncodings(images):
     encodeList = []
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)
+        encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
 
+encodeListKnown = faceEncodings(images)
+print("all encodings complete")
 
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-def captureScreen(bbox=(300,300,690+300,530+300)):
-    capScr = np.array(ImageGrab.grab(bbox))
-    capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-    facesCurFrame = face_recognition.face_locations(capScr)
-    encodesCurFrame = face_recognition.face_encodings(capScr, facesCurFrame)
-    return capScr
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+def attendance(name):
+    df = pd.read_csv('Attendance.csv')
+    print(df)
+
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for Line in myDataList:
+            entry = Line.split(',')
+            nameList.append(entry[0])
+
+
+        if name not in nameList:
+            time_now = datetime.now()
+            tStr = time_now.strftime('%H:%M:%S')
+            dStr = time_now.strftime('%d/%m/%Y')
+            f.writelines(f'{name},{tStr},{dStr}')
+
+        print(nameList)
+        print(name)
+
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    faces = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+    faces = cv2.cvtColor(faces, cv2.COLOR_BGR2RGB)
+
+    facesCurrentFrame = face_recognition.face_locations(faces)
+    encodesCurrentFrame = face_recognition.face_encodings(faces, facesCurrentFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis)
 
-encodeListKnown = findEncodings(images)
-print('Encoding Complete')
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = personName[matchIndex].upper()
+            #print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+            attendance(name)
+    cv2.imshow("camera", frame)
+    if cv2.waitKey(10) == 13:
+        break
+
+df = pd.read_csv('Attendance.csv')
+
+print(df.to_string())
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
